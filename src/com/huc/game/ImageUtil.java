7f8450e6754f383c2b58a91ea6179a2e5426f69d
QianXia <39728106+L0serQianXia@users.noreply.github.com>
2026-06-15 19:25:23 +0800

    修复编译后的文件资源无法找到的问题

--- src/com/huc/game/ImageUtil.java
+++ src/com/huc/game/ImageUtil.java
@@ -3,19 +3,42 @@ package com.huc.game;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ImageUtil {
+    private static boolean isDev;
+
+    static {
+        try {
+            URL url = ImageUtil.class.getProtectionDomain().getCodeSource().getLocation();
+            if (new File(url.toURI()).isDirectory()) {
+                isDev = true;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     //通过图片名，读取图片对象的方法
     public static Image getImage(String name) {
-        File f = new File("image/"+name);
         try {
-            URL url = f.toURI().toURL();
-            return ImageIO.read(url);
+            Image image;
+            if (isDev) {
+                File file = new File("image/" + name);
+                URL url = file.toURI().toURL();
+                image = ImageIO.read(url);
+            } else {
+                InputStream in = ImageUtil.class.getResourceAsStream("/image/" + name);
+                image = ImageIO.read(in);
+            }
+            return image;
         } catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
         return null;
     }
